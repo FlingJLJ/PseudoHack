ac19b0309ffaba076c30dd78688b8c7215902c22
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-11-06 06:31:32 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include <ctime>
 using namespace std;
 
-static void showScene(unsigned char scene)
+int main()
 {
-    if (scene == 0)
+    srand(time(0));
+    while (1)
     {
         for (int i = 0; i < 50000; i++)
         {
@@ -19,25 +20,6 @@ static void showScene(unsigned char scene)
                 cout << rand()% 2;
             }
         }
-    }
-    if (scene == 1)
-    {
-        for (int i = 0; i < 50; i++)
-        {
-            cout << "Stealing passwords from " << rand()% 255 << "." << rand()% 255 << "." << rand()% 255 << "." << rand()% 255 << "\n";
-            Sleep(100);
-        }
-        
-    }
-}
-
-int main()
-{
-    srand(time(0));
-    while (1)
-    {
-        showScene(rand()% 3);
-        system("cls");
     }
     return 0;
 }
