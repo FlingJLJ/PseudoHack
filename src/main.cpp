baccde82b42f26994235ec573fea41ff38798572
FlingJLJ <60308125+FlingJLJ@users.noreply.github.com>
2020-11-07 22:08:20 -0600

    e

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,13 @@ int main()
     srand(time(0));
     while (1)
     {
-        for (int i = 0; i < 50000; i++)
+        if (rand()% 3 == 0)
         {
-            if (rand()% 3 == 0)
-            {
-                cout << " ";
-            }
-            else
-            {
-                cout << rand()% 2;
-            }
+            cout << " ";
+        }
+        else
+        {
+            cout << rand()% 2;
         }
     }
     return 0;
